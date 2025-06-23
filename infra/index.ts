c784fcf7e9bb7933066e7da8bb694679fb0e986f
Pedro H. Farolfi <phfarolfi@gmail.com>
2025-06-22 22:11:36 -0300

    build: create cluster with app orders image

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,47 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+const ordersECRRepository = new awsx.ecr.Repository("orders-ecr", {
+    forceDelete: true, // Ensures the repository is deleted when the stack is destroyed
+})
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const ordersECRToken = aws.ecr.getAuthorizationTokenOutput({
+    registryId: ordersECRRepository.repository.registryId,
+});
+
+export const ordersDockerImage = new docker.Image("orders-image", {
+    tags: [
+        pulumi.interpolate`${ordersECRRepository.repository.repositoryUrl}:latest`,
+    ],
+    context: {
+        location: '../app-orders',
+    },
+    push: true,
+    platforms: [
+        'linux/amd64'
+    ],
+    registries: [
+        {
+            address: ordersECRRepository.repository.repositoryUrl,
+            username: ordersECRToken.userName,
+            password: ordersECRToken.password,
+        }
+    ]
+})
+
+const cluster = new awsx.classic.ecs.Cluster("app-cluster")
+
+const ordersService = new awsx.classic.ecs.FargateService("fargate-orders", {
+    cluster,
+    desiredCount: 1,
+    waitForSteadyState: false,
+    taskDefinitionArgs: {
+        container: {
+            image: ordersDockerImage.ref,
+            cpu: 256,
+            memory: 512,
+        },
+    },
+})
